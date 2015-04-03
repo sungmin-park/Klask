c6818c6871f0eaa454860d8a59a593e160b399d0
Sung Min Park <vamf12@gmail.com>
2015-04-03 11:52:04 +0900

    add redirect land test

--- src/test/kotlin/com/klask/server.kt
+++ src/test/kotlin/com/klask/server.kt
@@ -1,7 +1,10 @@
 package com.klask.server
 
 import com.klask.Klask
+import com.klask.RedirectResponse
 import com.klask.router.Route
+import com.klask.shorthands.redirect
+import com.klask.urlfor.urlfor
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -14,8 +17,18 @@ object app : Klask() {
     }
 
     Route("/static/<fileName:path>")
-    override fun static(fileName: String):String {
+    override fun static(fileName: String): String {
         throw IllegalArgumentException(fileName)
+    }
+
+    Route("/redirect")
+    fun redirect(): RedirectResponse {
+        return redirect(urlfor("redirectLand"))
+    }
+
+    Route("/redirect/land")
+    fun redirectLand(): String {
+        return "land"
     }
 }
 
@@ -38,5 +51,10 @@ class ServerTest {
     Test
     fun testStatic() {
         Assert.assertEquals("welcome", app.server.get("/static/welcome.txt"))
+    }
+
+    Test
+    fun testRedirect() {
+        Assert.assertEquals("land", app.server.get("/redirect"))
     }
 }
